e5ac6b544fe66a3373cc70b5b4cc527d14a8289e
sm1lla <foxsmilla@gmail.com>
2024-05-28 12:29:47 +0200

    webhook

--- webhook.js
+++ webhook.js
@@ -1,24 +1,32 @@
 // app.js
 const express = require("express");
 const bodyParser = require("body-parser");
+const simpleGit = require("simple-git");
+const git = simpleGit.default();
+
 const app = express();
 const PORT = 80;
 // Middleware to parse JSON bodies
 app.use(bodyParser.json());
 
+async function git_pull() {
+    await git.pull()
+}
+
 // Webhook endpoint
 app.post("/webhook", (req, res) => {
     const event = req.body;
     // Process the event
-    console.log("Received event:", event);
+    //console.log("Received event:", event);
     // Example: Handle different event types
     
     // Respond to acknowledge receipt of the event
     res.status(202).send('Accepted');
 
-    const githubEvent = request.headers['x-github-event'];
+    const githubEvent = req.headers['x-github-event'];
     if (githubEvent == "push") {
         console.log("New changes.")
+        git_pull();
     }
 });
 
